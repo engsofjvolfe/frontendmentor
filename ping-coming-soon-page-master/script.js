c0d97bebf0188d9fe7b1c1f8db5a7750fdbfc8cd
Jeanco Volfe <engsofjvolfe@gmail.com>
2022-11-29 11:10:09 -0300

    incomplete JS

---
+++ ping-coming-soon-page-master/script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('#form');
+const email = document.querySelector('#email');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const emailValue = email.value;
+    if (emailValue === "") {
+        setErrorFor(email, "Plase provide a valid email address");
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, "Plase provide a valid email address");
+    } else {
+        setSuccessFor(email);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formcontrol.QuerySelector('small');
+    formControl.className = "form-control error";
+    small.innerText = message;
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
