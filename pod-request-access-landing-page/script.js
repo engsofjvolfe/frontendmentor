047d224eb11595a0b1c50e30fe0a160cf54050b1
Jeanco Volfe <engsofjvolfe@gmail.com>
2022-12-06 15:25:25 -0300

    JS email validation

---
+++ pod-request-access-landing-page/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const emailValue = email.value;
+
+    if (emailValue === "") {
+        setErrorFor("Fields can not be empty!");
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor("Oops! Please check your email!");
+    } else {
+        setSuccessFor();
+    }
+}
+
+function setErrorFor(message) {
+    const formControl = document.querySelector(".form-control");
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+}
+
+function setSuccessFor() {
+    const formControl = document.querySelector(".form-control");
+    formControl.className = "form-control success";
+}
+
+// REGEX
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
