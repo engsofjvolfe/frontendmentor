4e4f26778c5f2df7fbff4fc0716c1895144e7c47
Jeanco Volfe <engsofjvolfe@gmail.com>
2022-12-30 14:23:09 -0300

    JS email validation

---
+++ maker-pre-launch-landing-page/script.js
@@ -0,0 +1,39 @@
+const forms = document.querySelector('#forms');
+const email = document.querySelector('#email');
+
+forms.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const emailValue = email.value;
+
+    if (emailValue === "") {
+        email.focus();
+        setErrorFor(email, "Oops! That doesn’t look like an email address");
+    } else if (!checkEmail(emailValue)) {
+        email.focus();
+        setErrorFor(email, "Oops! That doesn’t look like an email address")
+    } else {
+        console.log("sucesso")
+        setSuccessFor(email);
+    }
+
+    function setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector('small');
+        formControl.className = "form-control error";
+        small.innerText = message;
+    }
+
+    function setSuccessFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = "form-control success";
+    }
+
+
+    function checkEmail(email) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
+}
