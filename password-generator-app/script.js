1afd0307e967fe842a9824b0a75446208f103d66
Jeanco Volfe <engsofjvolfe@gmail.com>
2023-02-05 13:24:30 -0300

    Class added to  #generated-pwd-container using JS - outline

--- password-generator-app/script.js
+++ password-generator-app/script.js
@@ -6,64 +6,70 @@ const generatedPwd = document.getElementById('generated-pwd-container')
 
 //SLIDER
 slider.addEventListener('input', function () {
-  // Add an event listener to the slider to listen for input events
-  const percent = (slider.value - slider.min) / (slider.max - slider.min) // Calculate the percentage of the slider value relative to its min and max values
-  slider.style.background = `linear-gradient(to right, #a4ffaf ${
-    percent * 100
-  }%, #24232c 0%)` // Set the background of the slider to a gradient, with #a4ffaf at the position represented by the percent value, and #24232c at 0%
-
-  characterLength.innerHTML = slider.value // Set the value of the element with id character-length to the current value of the slider
+    // Add an event listener to the slider to listen for input events
+    const percent = (slider.value - slider.min) / (slider.max - slider.min) // Calculate the percentage of the slider value relative to its min and max values
+    slider.style.background = `linear-gradient(to right, #a4ffaf ${percent * 100
+        }%, #24232c 0%)` // Set the background of the slider to a gradient, with #a4ffaf at the position represented by the percent value, and #24232c at 0%
+    characterLength.innerHTML = slider.value // Set the value of the element with id character-length to the current value of the slider
 })
 //END SLIDER
 
 btn.addEventListener('click', function () {
-  let selectedOptions = []
-  // loop through all checkboxes to see which ones are selected
-  checkboxes.forEach(function (checkbox) {
-    if (checkbox.checked) {
-      selectedOptions.push(checkbox.value)
+    let selectedOptions = []
+    // loop through all checkboxes to see which ones are selected
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            selectedOptions.push(checkbox.value)
+        }
+    })
+    //END CHECKBOXES
+
+    let password = '' // Initialize the `password` variable as an empty string
+
+    // The following loop will be executed the number of times specified in the value of the slider
+    for (let i = 0; i < slider.value; i++) {
+        // Choose a random option from the values in `selectedOptions`
+        let selectedOption =
+            selectedOptions[Math.floor(Math.random() * selectedOptions.length)]
+
+        // Depending on the chosen option, do the following:
+        switch (selectedOption) {
+            case 'lower':
+                // Add a random lowercase letter character to the `password` variable
+                // The ASCII code for lowercase letters starts at 97, so we use `Math.floor(Math.random() * 26) + 97` to generate a random number between 97 and 122
+                // Then we convert that number to a lowercase letter using `String.fromCharCode`
+                password += String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+                break
+            case 'upper':
+                // Add a random uppercase letter character to the `password` variable
+                // The ASCII code for uppercase letters starts at 65, so we use `Math.floor(Math.random() * 26) + 65` to generate a random number between 65 and 90
+                // Then we convert that number to an uppercase letter using `String.fromCharCode`
+                password += String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+                break
+            case 'number':
+                // Add a random number character to the `password` variable
+                // The ASCII code for numbers starts at 48, so we use `Math.floor(Math.random() * 10) + 48` to generate a random number between 48 and 57
+                // Then we convert that number to a number using `String.fromCharCode`
+                password += String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+                break
+            case 'symbol':
+                // Add a random symbol character to the `password` variable
+                let specialChars = '!@#$%^&*(){}[]=<>/,.' // Define a string with the available symbols
+                // Choose a random symbol from the `specialChars` string and add it to the `password` variable
+                password += specialChars[Math.floor(Math.random() * specialChars.length)]
+                break
+        }
+        // Finally, assign the value of the `password` variable to
+        generatedPwd.innerHTML = password
+
+        // Check if password has been generated or inserted
+        if (generatedPwd) {
+            // Add 'has-password' class to the generate-pwd element
+            generatedPwd.classList.add('has-password');
+        } else {
+            // Remove 'has-password' class from the generate-pwd element
+            generatedPwd.classList.remove('has-password');
+        }
     }
-  })
-//END CHECKBOXES
-
-  let password = '' // Initialize the `password` variable as an empty string
-
-  // The following loop will be executed the number of times specified in the value of the slider
-  for (let i = 0; i < slider.value; i++) {
-    // Choose a random option from the values in `selectedOptions`
-    let selectedOption =
-      selectedOptions[Math.floor(Math.random() * selectedOptions.length)]
-
-    // Depending on the chosen option, do the following:
-    switch (selectedOption) {
-      case 'lower':
-        // Add a random lowercase letter character to the `password` variable
-        // The ASCII code for lowercase letters starts at 97, so we use `Math.floor(Math.random() * 26) + 97` to generate a random number between 97 and 122
-        // Then we convert that number to a lowercase letter using `String.fromCharCode`
-        password += String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-        break
-      case 'upper':
-        // Add a random uppercase letter character to the `password` variable
-        // The ASCII code for uppercase letters starts at 65, so we use `Math.floor(Math.random() * 26) + 65` to generate a random number between 65 and 90
-        // Then we convert that number to an uppercase letter using `String.fromCharCode`
-        password += String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-        break
-      case 'number':
-        // Add a random number character to the `password` variable
-        // The ASCII code for numbers starts at 48, so we use `Math.floor(Math.random() * 10) + 48` to generate a random number between 48 and 57
-        // Then we convert that number to a number using `String.fromCharCode`
-        password += String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-        break
-      case 'symbol':
-        // Add a random symbol character to the `password` variable
-        let specialChars = '!@#$%^&*(){}[]=<>/,.' // Define a string with the available symbols
-        // Choose a random symbol from the `specialChars` string and add it to the `password` variable
-        password += specialChars[Math.floor(Math.random() * specialChars.length)]
-        break
-    }
-    generatedPwd.innerHTML = password     
-  }
-
-  // Finally, assign the value of the `password` variable to
 })
 
